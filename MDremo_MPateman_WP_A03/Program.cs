6caa3b610054b31e65bf82f2c0b234e225e607b6
Max Pateman <maxpateman22@gmail.com>
2021-10-04 15:34:45 -0400

    added some (potentially) helpful comments for collaboration.

--- MDremo_MPateman_WP_A03/Program.cs
+++ MDremo_MPateman_WP_A03/Program.cs
@@ -6,9 +6,10 @@ namespace MDremo_MPateman_WP_A03
     {
         static void Main(string[] args)
         {
+            /*When making changes, ensure you pull the file, make changes, then commit to repository.*/
             Console.WriteLine("Hello World!");
             Console.WriteLine("This thing sucks...");
-            Console.WriteLine("This thing sucks...");
+            Console.WriteLine("Okay, I may have figured this out.");
         }
     }
 }
