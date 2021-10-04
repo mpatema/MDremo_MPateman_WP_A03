3f219a4105ff6ecfa452082a7a177b82450d014c
maxpa <maxpa@LAPTOP-9NSM9D4N>
2021-10-04 15:20:40 -0400

    Add project files.

---
+++ MDremo_MPateman_WP_A03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDremo_MPateman_WP_A03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
