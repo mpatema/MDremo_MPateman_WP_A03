8f762db8990937a5b11e6bab22c647513b520c4b
Dremm4549 <mDremo5093@conestogac.on.ca>
2021-10-05 18:14:31 -0400

    added the list ds

--- MDremo_MPateman_WP_A03/Program.cs
+++ MDremo_MPateman_WP_A03/Program.cs
@@ -1,4 +1,8 @@
-﻿beusing System;
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
 
 namespace MDremo_MPateman_WP_A03
 {
@@ -6,10 +10,37 @@ namespace MDremo_MPateman_WP_A03
     {
         static void Main(string[] args)
         {
-            /*When making changes, ensure you pull the file, make changes, then commit to repository.*/
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This thing sucks...");
-            Console.WriteLine("Okay, I may have figured this oufsdfsft.");
+            string fp = @"C:\temp\Sample.txt";
+            StreamReader sr = new StreamReader(fp);
+            string line = null;
+            string[] tempWords;
+            Stopwatch sw = new Stopwatch();
+
+
+            List<string> wordList = new List<string>();
+
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                line.Trim();
+                tempWords = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < tempWords.Length; i++)
+                {
+                    wordList.Add(tempWords[i]);
+                    sw.Start();
+                    //Console.WriteLine(tempWords[i]);                   
+                }
+            }
+            sw.Stop();
+            sr.Close();
+            Console.WriteLine($"Execution time  {sw.ElapsedMilliseconds}ms");
+            foreach (string str in wordList)
+            {
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
