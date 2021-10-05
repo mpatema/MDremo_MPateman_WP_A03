19c18c80ce544b2fa4a091e35e5f3a569f226ad8
Dremm4549 <mDremo5093@conestogac.on.ca>
2021-10-05 18:13:11 -0400

    Hello

--- MDremo_MPateman_WP_A03/Program.cs
+++ MDremo_MPateman_WP_A03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿beusing System;
 
 namespace MDremo_MPateman_WP_A03
 {
@@ -9,7 +9,7 @@ namespace MDremo_MPateman_WP_A03
             /*When making changes, ensure you pull the file, make changes, then commit to repository.*/
             Console.WriteLine("Hello World!");
             Console.WriteLine("This thing sucks...");
-            Console.WriteLine("Okay, I may have figured this out.");
+            Console.WriteLine("Okay, I may have figured this oufsdfsft.");
         }
     }
 }
