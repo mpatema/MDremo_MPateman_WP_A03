100d90d1e1cd54bc26afbaca78e27b99473774c7
Dremm4549 <mDremo5093@conestogac.on.ca>
2021-10-05 20:07:43 -0400

    fixed the backslash with extra quotes

--- MDremo_MPateman_WP_A03/Program.cs
+++ MDremo_MPateman_WP_A03/Program.cs
@@ -10,36 +10,37 @@ namespace MDremo_MPateman_WP_A03
     {
         static void Main(string[] args)
         {
-            string fp = @"C:\temp\Sample.txt";
+            string fp = @"C:\temp\sampleText.txt";
             StreamReader sr = new StreamReader(fp);
             string line = null;
             string[] tempWords;
             Stopwatch sw = new Stopwatch();
-
-
+                   
             List<string> wordList = new List<string>();
-
 
             while (!sr.EndOfStream)
             {
                 line = sr.ReadLine();
                 line.Trim();
-                tempWords = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                tempWords = line.Split(' ','"',StringSplitOptions.RemoveEmptyEntries);
                 for (int i = 0; i < tempWords.Length; i++)
                 {
-                    wordList.Add(tempWords[i]);
+                    if (tempWords[i].StartsWith('"'))
+                    {
+                        tempWords[i] = tempWords[i].TrimStart('"');
+                    }
+                    else if (tempWords[i].EndsWith('"'))
+                    {
+                        tempWords[i] = tempWords[i].TrimEnd('"');
+                    }
                     sw.Start();
-                    //Console.WriteLine(tempWords[i]);                   
+                    wordList.Add(tempWords[i]);                                                 
                 }
             }
+            //wordList.Sort();
             sw.Stop();
             sr.Close();
-            Console.WriteLine($"Execution time  {sw.ElapsedMilliseconds}ms");
-            foreach (string str in wordList)
-            {
-            }
-
-
+            Console.WriteLine($"Execution time  {sw.ElapsedMilliseconds}ms");         
             Console.ReadLine();
         }
     }
